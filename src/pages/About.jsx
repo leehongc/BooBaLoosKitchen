01ec0404fc489f65a5a13b65a55f9441913e9890
Hong C <36454250+leehongc@users.noreply.github.com>
2024-12-05 07:54:14 -0800

    minor stylizing change for About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,11 +3,13 @@ import { Container } from '../components/layout/Container';
 const About = () => {
   return (
     <Container>
-      <h1>About BooBaLoo's Kitchen</h1>
-      <p>
-        Welcome to BooBaLoo's Kitchen! This site is a personal collection of recipes that we love and enjoy making.
-        It reflects our passion for cooking and sharing the joy of food. Thank you for visiting, and happy cooking!
-      </p>
+      <header className="py-16 text-center max-w-2xl mx-auto">
+      <h1 className="font-serif text-4xl md:text-5xl mb-6">About BooBaLoo's Kitchen</h1>
+        <p className="text-gray-600 text-lg">
+          Welcome to BooBaLoo's Kitchen! This site is a personal collection of recipes that we love and enjoy making.
+          It reflects our passion for cooking and sharing the joy of food. Thank you for visiting, and happy cooking!
+        </p>
+      </header>
     </Container>
   );
 };
