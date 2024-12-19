bf42b519a94ba9dcde9f3dff8cddf8e1653b5f5d
Hong C <36454250+leehongc@users.noreply.github.com>
2024-12-18 17:50:30 -0800

    update vite config to expose app to local network

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This binds the server to all network interfaces
+  },
 })
