81ac2d70d42dc9af1dd4a06a7efbe83e205abf30
Hong C <36454250+leehongc@users.noreply.github.com>
2024-12-21 21:42:05 -0800

    configure github pages for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/BooBaLoosKitchen/',
   plugins: [react()],
   server: {
     host: true, // This binds the server to all network interfaces
